7497d8cbdb37cce13f960b9814dff23eaf1d0b54
otrack <0track@gmail.com>
2016-01-08 15:34:11 +0100

    [FIX] scripts

--- src/main/bin/setup.sh
+++ src/main/bin/setup.sh
@@ -1,5 +1,12 @@
-#!/bin/sh
-hadoop fs -put -f inject /
+#!/bin/bash
+export NUTCHDIR="/home/ubuntu/nutch"
+source ${NUTCHDIR}/bin/config.sh
+
+hadoop fs -rm -r -f /inject
+hadoop fs -put -f ${NUTCHDIR}/inject /
+
+hadoop fs -rm -r -f /nutch
 hadoop fs -mkdir /nutch
-hadoop fs -put -f lib /nutch
-hadoop fs -put -f plugin /
+hadoop fs -put -f ${NUTCHDIR}/lib /nutch
+hadoop fs -put -f ${NUTCHDIR}/plugin /
+hadoop fs -rm /nutch/lib/nutch-2.2.jar
--- src/main/bin/unicrawl.sh
+++ src/main/bin/unicrawl.sh
@@ -1,22 +1,11 @@
 #!/bin/bash
-
-export NUTCHDIR="/opt/nutch"
+export NUTCHDIR="/home/ubuntu/nutch"
 source ${NUTCHDIR}/bin/config.sh
-uclouds=(leads-qe3 leads-qe8 leads-qe28)
 
-function stopExp(){
-    pkill -TERM $(ps aux | grep 'unicrawl.sh' | grep -v grep | awk '{print $2}') 2&>1 > /dev/null
-    exit 0
-}
-
-
+uclouds=(80.156.73.93 5.147.254.197)
 
 for ucloud in ${uclouds[@]}
 do
     ssh ${ucloud} "cd ${NUTCHDIR} && ./bin/dnutch $1" > ${NUTCHDIR}/ucloud_${ucloud}.txt &
 done;
 wait
-
-trap "stopExp; wait; exit 255" SIGINT SIGTERM
-
-exit 0
